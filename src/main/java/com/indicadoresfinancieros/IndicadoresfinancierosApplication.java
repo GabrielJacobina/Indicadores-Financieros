4bdeda5e9d490a88f11fa270aaa955571d42ccd2
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-12-16 22:16:02 -0300

    first commit

---
+++ src/main/java/com/indicadoresfinancieros/IndicadoresfinancierosApplication.java
@@ -0,0 +1,13 @@
+package com.indicadoresfinancieros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicadoresfinancierosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndicadoresfinancierosApplication.class, args);
+    }
+
+}
