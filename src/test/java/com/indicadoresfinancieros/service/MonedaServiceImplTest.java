9a06a57d752196929f5d4361f5a6417e28fc74e3
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-01-22 01:43:54 -0300

    testing methods of saving

--- src/test/java/com/indicadoresfinancieros/service/MonedaServiceImplTest.java
+++ src/test/java/com/indicadoresfinancieros/service/MonedaServiceImplTest.java
@@ -3,6 +3,7 @@ package com.indicadoresfinancieros.service;
 import com.indicadoresfinancieros.document.Moneda;
 import com.indicadoresfinancieros.repository.MonedaRepository;
 import com.indicadoresfinancieros.util.MonedaCreator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,16 +35,75 @@ class MonedaServiceImplTest {
 
     private final Moneda  moneda = MonedaCreator.createValidMoneda();
 
+    @BeforeAll
+    public static void blockHoundSetup() {
+        BlockHound.install();
+    }
+
+    @Test
+    public void blockHoundWorks() {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
     @BeforeEach
     public void setUp() {
         BDDMockito.when(monedaRepositoryMock.findById(ArgumentMatchers.anyString()))
                 .thenReturn(Mono.just(moneda));
+
+        BDDMockito.when(monedaRepositoryMock.save(MonedaCreator.createMonedaToBeSaved()))
+                .thenReturn(Mono.just(moneda));
     }
 
     @Test
-    @DisplayName("findByData returns Mono with moneda when it exists")
-    public void findByData_ReturnMonoMoneda_whenSuccesful() {
+    @DisplayName("findByData returns Mono with moneda when Successful")
+    public void findByData_ReturnMonoMoneda_whenSuccessful() {
         StepVerifier.create(monedaService.findByData("10-01-2021"))
+                .expectSubscription()
+                .expectNext(moneda)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("save creates a Moneda in the database when successful")
+    public void save_CreatesMonedaInTheDatabase_whenSuccessful() {
+        Moneda monedaToBeSaved = MonedaCreator.createMonedaToBeSaved();
+        StepVerifier.create(monedaService.save(monedaToBeSaved))
+                .expectSubscription()
+                .expectNext(moneda)
+                .verifyComplete();
+    }
+
+    /**
+     * Test is returning null
+     */
+    @Test
+    @DisplayName("salvarMoneda creates an Moneda when Successful")
+    public void salvarMoeda_CreatesMoneda_whenSuccessful() {
+        LocalDate data = LocalDate.of(2021, 01, 01);
+        String dataBr = data.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        String dataS = data.toString();
+
+        StepVerifier.create(monedaService.salvarMoneda(dataBr, dataS))
+                .expectSubscription()
+                .expectNext(moneda)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("obtendoMoneda creates a Moneda in the database when successful")
+    public void obtendoMoneda_CreatesMonedaInTheDatabase_whenSuccessful() {
+        StepVerifier.create(monedaService.obtendoMoneda(LocalDate.now()))
                 .expectSubscription()
                 .expectNext(moneda)
                 .verifyComplete();
--- src/test/java/com/indicadoresfinancieros/util/MonedaCreator.java
+++ src/test/java/com/indicadoresfinancieros/util/MonedaCreator.java
@@ -4,19 +4,21 @@ import com.indicadoresfinancieros.document.Indicators;
 import com.indicadoresfinancieros.document.Moneda;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class MonedaCreator {
 
     public static Moneda createMonedaToBeSaved() {
         return Moneda.builder()
+                .date(LocalDate.now().toString())
                 .indicators(new Indicators(new BigDecimal("5.4"),new BigDecimal("7.34"), new BigDecimal("6.26")))
                 .build();
     }
 
     public static Moneda createValidMoneda() {
         return Moneda.builder()
-                .date("10-01-2021")
-                .indicators(new Indicators(new BigDecimal("5.4"),new BigDecimal("7.34"), new BigDecimal("6.26")))
+                .date("2021-01-01")
+                .indicators(new Indicators(new BigDecimal("29069.39"),new BigDecimal("50978"), new BigDecimal("717.400857")))
                 .build();
     }
 }
